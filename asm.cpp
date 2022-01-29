443740ff2647af729f7fef4e3592ad6cb00516bd
dev1stack <duonghvph29740@fpt.edu.vn>
2022-01-29 20:59:49 +0700

    asm

--- asm.cpp
+++ asm.cpp
@@ -2,18 +2,61 @@
 
 int main(){
 	int fun;
-	printf("Moi ban chon chuc nang bang cach nhap so:");
+	printf("Moi ban chon chuc nang bang cach nhap so:\n");
 	printf("1. Kiem tra so nguyen\n");
-	printf("2. Tim uoc chung va boi chung cua 2 so");
-	printf("3. Tinh toan tien cho quan karaoke");
-	printf("4. Tinh tien dien");
-	printf("5. Doi tien");
-	printf("6. Tinh lai suat ngan hang vay tra gop");
-	printf("7. Vay tien mua xe");
-	printf("8. Sap xep thong tin sinh vien");
-	printf("9. Game POLY-LOTT");
-	printf("10. Tinh toan phan so");
+	printf("2. Tim uoc chung va boi chung cua 2 so\n");
+	printf("3. Tinh toan tien cho quan karaoke\n");
+	printf("4. Tinh tien dien\n");
+	printf("5. Doi tien\n");
+	printf("6. Tinh lai suat ngan hang vay tra gop\n");
+	printf("7. Vay tien mua xe\n");
+	printf("8. Sap xep thong tin sinh vien\n");
+	printf("9. Game POLY-LOTT\n");
+	printf("10. Tinh toan phan so\n");
 	
-	
+	scanf("%d", &fun);
+	switch(fun){
+		case 1:{
+			printf("1. Kiem tra so nguyen\n");
+			break;
+		}
+		case 2:{
+			printf("2. Tim uoc chung va boi chung cua 2 so\n");
+			break;
+		}
+		case 3:{
+			printf("3. Tinh toan tien cho quan karaoke\n");
+			break;
+		}
+		case 4:{
+			printf("4. Tinh tien dien\n");
+			break;
+		}
+		case 5:{
+			printf("5. Doi tien\n");
+			break;
+		}
+		case 6:{
+			printf("6. Tinh lai suat ngan hang vay tra gop\n");
+			break;
+		}
+		case 7:{
+			printf("7. Vay tien mua xe\n");
+			break;
+		}
+		case 8:{
+			printf("8. Sap xep thong tin sinh vien\n");
+			break;
+		}
+		case 9:{
+			printf("9. Game POLY-LOTT\n");
+			break;
+		}
+		case 10:{
+			printf("10. Tinh toan phan so\n");
+			break;
+		}
+		
+	}
 	
 }
